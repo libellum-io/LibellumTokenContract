f64777a5d0a9bb9b37632225df635d4411d9e656
stanko-culaja-libellum <stanko@libellum.io>
2018-08-28 13:20:52 +0200

    Added some unit tests just to proove that totalSupply is correct and that basic transfer functionality works.

--- test/LibellumCoinTests.js
+++ test/LibellumCoinTests.js
@@ -1,19 +1,33 @@
 var LibellumCoin = artifacts.require("./LibellumCoin.sol");
+const BigNumber = web3.BigNumber;
+
+const TOTAL_SUPPLY = 100000000 * (10 ** 18);
+
+require('chai')
+  .use(require('chai-bignumber')(BigNumber))
+    .should();
 
 contract('LibellumCoin', function (accounts) {
     var contract;
-    beforeEach(function () {
-        return LibellumCoin.new()
-            .then(function(instance) {
-                contract = instance;
-            });
+    beforeEach(async function () {
+        this.token = await LibellumCoin.new();
     });
 
-    it("test ERC20 totalSupply()", function () {
-        return LibellumCoin.deployed().then(function (instance) {
-            return contract.totalSupply.call({from: accounts[0]});
-        }).then(function (totalSupply) {
-            assert.equal(totalSupply.valueOf(), 100000000 * (10 ** 18), "total supply should be 100Mio");
+    describe('total supply', function () {
+        it('returns the total amount of tokens', async function () {
+            (await this.token.totalSupply()).should.be.bignumber.equal(TOTAL_SUPPLY);
+        });
+    });
+
+    describe('transfer', function () {
+        describe('when the recepient is valid address', function () {
+            it('10 LIB moved from sender to recepient', async function () {
+                let amountToTransfer = 10 * (10 ** 18);
+                (await this.token.transfer(accounts[1], amountToTransfer, {from: accounts[0]}));
+    
+                (await this.token.balanceOf(accounts[0])).should.be.bignumber.equal(TOTAL_SUPPLY - amountToTransfer);
+                (await this.token.balanceOf(accounts[1])).should.be.bignumber.equal(amountToTransfer);
+            });
         });
     });
 });
