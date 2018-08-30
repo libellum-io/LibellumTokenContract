53cb6412a94b737fa837afad6ff3eb08102b088d
stanko-culaja-libellum <stanko@libellum.io>
2018-08-30 13:01:14 +0200

    Added LibellumTokenTImelock contract.

--- migrations/2_deploy_libellum_contract.js
+++ migrations/2_deploy_libellum_contract.js
@@ -1,8 +1,14 @@
+const LibellumTokenTimelock = artifacts.require('./contracts/LibellumTokenTimelock.sol');
 const LibellumCoin = artifacts.require('./contracts/LibellumCoin.sol');
 
 module.exports = function(deployer, network, accounts) {
+    let _1_1_2019_time = 1546300800;
+
     return deployer
         .then(() => {
             return deployer.deploy(LibellumCoin, {from: accounts[0]});
+        })
+        .then(() => {
+            return deployer.deploy(LibellumTokenTimelock, accounts[1], _1_1_2019_time, {from: accounts[0]});
         });
 };
