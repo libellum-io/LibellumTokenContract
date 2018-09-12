5cab739675828b59b9b4d470b5ce7177aee98314
Stanko Culaja <stanko.culaja@kolotree.com>
2018-09-12 12:35:36 +0200

    Added one more test to check distribution lock addresses.

--- test/distribution/LockedTokenDistributionTests.js
+++ test/distribution/LockedTokenDistributionTests.js
@@ -15,6 +15,13 @@ contract('LockedTokenDistribution', function (accounts) {
         this.values = await LibellumTestValuesFrom(accounts, goal, individualCap);
     });
 
+    it ('founders and advisors addresses are correct', async function () {
+        (await this.values.libellumTokenDistribution.addresses.call(0)).should.be.equal(this.values.founder1);
+        (await this.values.libellumTokenDistribution.addresses.call(1)).should.be.equal(this.values.founder2);
+        (await this.values.libellumTokenDistribution.addresses.call(2)).should.be.equal(this.values.advisor1);
+        (await this.values.libellumTokenDistribution.addresses.call(3)).should.be.equal(this.values.advisor2);
+    });
+
     describe('when corwdsale is finalized (tokens are distributed)', function () {
         beforeEach(async function () {
             await this.values.increaseTimeToPhase1();
@@ -23,7 +30,7 @@ contract('LockedTokenDistribution', function (accounts) {
             await this.values.libellumCrowdsale.finalize();
         });
 
-        it('foundes and advisors balances contain correct funds', async function () {
+        it('founders and advisors balances contain correct funds', async function () {
             (await this.values.libellumToken.balanceOf(this.values.founder1)).should.be.bignumber.equal(2.5 * Mio * LIB);
             (await this.values.libellumToken.balanceOf(this.values.founder2)).should.be.bignumber.equal(2.5 * Mio * LIB);
             (await this.values.libellumToken.balanceOf(this.values.advisor1)).should.be.bignumber.equal(1.25 * Mio * LIB);
