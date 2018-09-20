320659f5a205c6d9365b398ceaa066f3f4e8787c
Stanko Culaja <stanko.culaja@kolotree.com>
2018-09-20 09:23:26 +0200

    Added compiler optimization and added rinkeby network to truffle.js

--- truffle.js
+++ truffle.js
@@ -1,9 +1,22 @@
 module.exports = {
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
   networks: {
       development: {
           host: "localhost",
           port: 8545,
-          network_id: "*" // Match any network id
+          network_id: "*", // Match any network id
+      },
+      rinkeby: {
+        host: "localhost",
+        port: 8545,
+        from: "0x0784b65432c5A5a75474BBdceeA2eDde45a19eE3",
+        network_id: 4,
+        gas: 7000000 // Gas limit used for deploys
       }
   }
 };
