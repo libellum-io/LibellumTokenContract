796611c0b9f5e9e55f3b53127d197fb74a6cf81e
Stanko Culaja <stanko.culaja@kolotree.com>
2018-09-21 09:40:55 +0200

    Added negative test for token distribution, when goal is not reached

--- test/distribution/LockedTokenDistributionTests.js
+++ test/distribution/LockedTokenDistributionTests.js
@@ -68,11 +68,8 @@ contract('LockedTokenDistribution', function (accounts) {
                 await this.values.libellumCrowdsale.finalize();
             });
     
-            it('founders and advisors balances contain zero LIBs', async function () {
-                (await this.values.libellumToken.balanceOf(this.values.founder1)).should.be.bignumber.equal(0 * LIB);
-                (await this.values.libellumToken.balanceOf(this.values.founder2)).should.be.bignumber.equal(0 * LIB);
-                (await this.values.libellumToken.balanceOf(this.values.advisor1)).should.be.bignumber.equal(0 * LIB);
-                (await this.values.libellumToken.balanceOf(this.values.advisor2)).should.be.bignumber.equal(0 * LIB);
+            it('distribution function cant be called from the original owner', async function () {
+                await expectThrow(this.values.libellumTokenDistribution.distribute());
             });
         });
     
@@ -82,70 +79,79 @@ contract('LockedTokenDistribution', function (accounts) {
                 await this.values.libellumCrowdsale.buyTokens(this.values.whitelistedBeneficiary, {value: goal, from: this.values.whitelistedBeneficiary});
                 await this.values.increaseTimeToAfterTheEnd();
                 await this.values.libellumCrowdsale.finalize();
+            });
+
+            it('distribute function can be executed by original owner', async function () {
                 await this.values.libellumTokenDistribution.distribute();
             });
-    
-            it('founders and advisors balances contain correct number of LIBs', async function () {
-                (await this.values.libellumToken.balanceOf(this.values.founder1)).should.be.bignumber.equal(2.5 * Mio * LIB);
-                (await this.values.libellumToken.balanceOf(this.values.founder2)).should.be.bignumber.equal(2.5 * Mio * LIB);
-                (await this.values.libellumToken.balanceOf(this.values.advisor1)).should.be.bignumber.equal(1.25 * Mio * LIB);
-                (await this.values.libellumToken.balanceOf(this.values.advisor2)).should.be.bignumber.equal(1.25 * Mio * LIB);
-            });
-    
-            describe('tocken timelocks', function () {
+
+            describe('after token distribution', function () {
                 beforeEach(async function () {
-                    this.founder1TockenTimelock = TokenTimelock.at(await this.values.libellumTokenDistribution.tokenTimelocks.call(0));
-                    this.founder2TockenTimelock = TokenTimelock.at(await this.values.libellumTokenDistribution.tokenTimelocks.call(1));
-                    this.advisor1TockenTimelock = TokenTimelock.at(await this.values.libellumTokenDistribution.tokenTimelocks.call(2));
-                    this.advisor2TockenTimelock = TokenTimelock.at(await this.values.libellumTokenDistribution.tokenTimelocks.call(3));
-                });
-                
-                it('founders and advisors timelocks contain correct funds', async function () {
-                    (await this.values.libellumToken.balanceOf(this.founder1TockenTimelock.address)).should.be.bignumber.equal(2.5 * Mio * LIB);
-                    (await this.values.libellumToken.balanceOf(this.founder2TockenTimelock.address)).should.be.bignumber.equal(2.5 * Mio * LIB);
-                    (await this.values.libellumToken.balanceOf(this.advisor1TockenTimelock.address)).should.be.bignumber.equal(1.25 * Mio * LIB);
-                    (await this.values.libellumToken.balanceOf(this.advisor2TockenTimelock.address)).should.be.bignumber.equal(1.25 * Mio * LIB);
+                    await this.values.libellumTokenDistribution.distribute();
                 });
 
-                describe('no time passed after the crowdsale', function () {
-                    it('founders are not able to release tokens', async function () {
-                        await expectThrow(this.founder1TockenTimelock.release({from: this.values.founder1}));
-                        await expectThrow(this.founder2TockenTimelock.release({from: this.values.founder2}));
-                        (await this.values.libellumToken.balanceOf(this.values.founder1)).should.be.bignumber.equal(2.5 * Mio * LIB);
-                        (await this.values.libellumToken.balanceOf(this.values.founder2)).should.be.bignumber.equal(2.5 * Mio * LIB);
-                    });
-
-                    it('advisors are not able to release tokens', async function () {
-                        await expectThrow(this.advisor1TockenTimelock.release({from: this.values.founder1}));
-                        await expectThrow(this.advisor2TockenTimelock.release({from: this.values.founder2}));
-                        (await this.values.libellumToken.balanceOf(this.values.advisor1)).should.be.bignumber.equal(1.25 * Mio * LIB);
-                        (await this.values.libellumToken.balanceOf(this.values.advisor2)).should.be.bignumber.equal(1.25 * Mio * LIB);
-                    });
+                it('founders and advisors balances contain correct number of LIBs', async function () {
+                    (await this.values.libellumToken.balanceOf(this.values.founder1)).should.be.bignumber.equal(2.5 * Mio * LIB);
+                    (await this.values.libellumToken.balanceOf(this.values.founder2)).should.be.bignumber.equal(2.5 * Mio * LIB);
+                    (await this.values.libellumToken.balanceOf(this.values.advisor1)).should.be.bignumber.equal(1.25 * Mio * LIB);
+                    (await this.values.libellumToken.balanceOf(this.values.advisor2)).should.be.bignumber.equal(1.25 * Mio * LIB);
                 });
-
-                describe('6 months passed after the crowdsale', function () {
+        
+                describe('tocken timelocks', function () {
                     beforeEach(async function () {
-                        await this.values.increaseTimeSixMonthsAfterTheEnd();
+                        this.founder1TockenTimelock = TokenTimelock.at(await this.values.libellumTokenDistribution.tokenTimelocks.call(0));
+                        this.founder2TockenTimelock = TokenTimelock.at(await this.values.libellumTokenDistribution.tokenTimelocks.call(1));
+                        this.advisor1TockenTimelock = TokenTimelock.at(await this.values.libellumTokenDistribution.tokenTimelocks.call(2));
+                        this.advisor2TockenTimelock = TokenTimelock.at(await this.values.libellumTokenDistribution.tokenTimelocks.call(3));
                     });
-
-                    it('advisors are able to release tokens', async function () {
-                        await this.advisor1TockenTimelock.release({from: this.values.founder1});
-                        await this.advisor2TockenTimelock.release({from: this.values.founder2});
-                        (await this.values.libellumToken.balanceOf(this.values.advisor1)).should.be.bignumber.equal(2.5 * Mio * LIB);
-                        (await this.values.libellumToken.balanceOf(this.values.advisor2)).should.be.bignumber.equal(2.5 * Mio * LIB);
+                    
+                    it('founders and advisors timelocks contain correct funds', async function () {
+                        (await this.values.libellumToken.balanceOf(this.founder1TockenTimelock.address)).should.be.bignumber.equal(2.5 * Mio * LIB);
+                        (await this.values.libellumToken.balanceOf(this.founder2TockenTimelock.address)).should.be.bignumber.equal(2.5 * Mio * LIB);
+                        (await this.values.libellumToken.balanceOf(this.advisor1TockenTimelock.address)).should.be.bignumber.equal(1.25 * Mio * LIB);
+                        (await this.values.libellumToken.balanceOf(this.advisor2TockenTimelock.address)).should.be.bignumber.equal(1.25 * Mio * LIB);
                     });
-                });
-
-                describe('one year passed after the crowdsale', function () {
-                    beforeEach(async function () {
-                        await this.values.increaseTimeOneYearAfterTheEnd();
+    
+                    describe('no time passed after the crowdsale', function () {
+                        it('founders are not able to release tokens', async function () {
+                            await expectThrow(this.founder1TockenTimelock.release({from: this.values.founder1}));
+                            await expectThrow(this.founder2TockenTimelock.release({from: this.values.founder2}));
+                            (await this.values.libellumToken.balanceOf(this.values.founder1)).should.be.bignumber.equal(2.5 * Mio * LIB);
+                            (await this.values.libellumToken.balanceOf(this.values.founder2)).should.be.bignumber.equal(2.5 * Mio * LIB);
+                        });
+    
+                        it('advisors are not able to release tokens', async function () {
+                            await expectThrow(this.advisor1TockenTimelock.release({from: this.values.founder1}));
+                            await expectThrow(this.advisor2TockenTimelock.release({from: this.values.founder2}));
+                            (await this.values.libellumToken.balanceOf(this.values.advisor1)).should.be.bignumber.equal(1.25 * Mio * LIB);
+                            (await this.values.libellumToken.balanceOf(this.values.advisor2)).should.be.bignumber.equal(1.25 * Mio * LIB);
+                        });
                     });
-
-                    it('founders are able to release tokens', async function () {
-                        await this.founder1TockenTimelock.release({from: this.values.founder1});
-                        await this.founder2TockenTimelock.release({from: this.values.founder2});
-                        (await this.values.libellumToken.balanceOf(this.values.founder1)).should.be.bignumber.equal(5 * Mio * LIB);
-                        (await this.values.libellumToken.balanceOf(this.values.founder2)).should.be.bignumber.equal(5 * Mio * LIB);
+    
+                    describe('6 months passed after the crowdsale', function () {
+                        beforeEach(async function () {
+                            await this.values.increaseTimeSixMonthsAfterTheEnd();
+                        });
+    
+                        it('advisors are able to release tokens', async function () {
+                            await this.advisor1TockenTimelock.release({from: this.values.founder1});
+                            await this.advisor2TockenTimelock.release({from: this.values.founder2});
+                            (await this.values.libellumToken.balanceOf(this.values.advisor1)).should.be.bignumber.equal(2.5 * Mio * LIB);
+                            (await this.values.libellumToken.balanceOf(this.values.advisor2)).should.be.bignumber.equal(2.5 * Mio * LIB);
+                        });
+                    });
+    
+                    describe('one year passed after the crowdsale', function () {
+                        beforeEach(async function () {
+                            await this.values.increaseTimeOneYearAfterTheEnd();
+                        });
+    
+                        it('founders are able to release tokens', async function () {
+                            await this.founder1TockenTimelock.release({from: this.values.founder1});
+                            await this.founder2TockenTimelock.release({from: this.values.founder2});
+                            (await this.values.libellumToken.balanceOf(this.values.founder1)).should.be.bignumber.equal(5 * Mio * LIB);
+                            (await this.values.libellumToken.balanceOf(this.values.founder2)).should.be.bignumber.equal(5 * Mio * LIB);
+                        });
                     });
                 });
             });
