9c172ad1d892c375501df00e9103e1dc529496c6
Stanko Culaja <stanko.culaja@kolotree.com>
2018-11-15 10:30:46 +0100

    Set new timing.

--- migrations/2_deploy_libellum_contract.js
+++ migrations/2_deploy_libellum_contract.js
@@ -4,10 +4,10 @@ const LibellumTokenDistribution = artifacts.require('./contracts/distribution/Li
 module.exports = function(deployer, network, accounts) {
     let goal = 1000000000000000000; // 1 ether
     let defaultIndividualCap = 1000000000000000000000; // 1000 ether
-    let openingTime = 1542272400; // 15-Nov-18, 9:00 AM
-    let phase1ToPhase2Time = 1543017600; // 24-Nov-18, 0:00 AM
-    let phase2ToPhase3Time = 1544054400; // 6-Dec-18, 0:00 AM
-    let closingTime = 1544871600; // 15-Dec-18, 11:00 AM
+    let openingTime = 1542366000; // 16-Nov-18, 11:00 AM GMT
+    let phase1ToPhase2Time = 1543017600; // 24-Nov-18, 0:00 AM GMT
+    let phase2ToPhase3Time = 1544054400; // 6-Dec-18, 0:00 AM GMT
+    let closingTime = 1544871600; // 15-Dec-18, 11:00 AM GMT
 
     let owner = accounts[0]; // use this for main network: '0x1C0b052eD15D706b4f954437403D17646a5c96C4';
     let founders = ['0xc8907621675711ed05b56d5f1ca98199d5EF13ba', '0xfE8Ff8d8d9F7d4357aD310f713A3CC770f16A079']
